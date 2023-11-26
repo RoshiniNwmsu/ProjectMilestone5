47215619df046b5d264ed0d7b9a844614c5c04da
RoshiniNwmsu <roshinibikkina14@gmail.com>
2023-11-25 22:17:51 -0600

    changed

---
+++ src/main/java/com/example/DMS/DogsManagmentSystem1Application.java
@@ -0,0 +1,13 @@
+package com.example.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagmentSystem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagmentSystem1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,12 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+/**
+*
+* @author Roshini Bikkina
+*/
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
